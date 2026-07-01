7d7d462637185a3caa4bac3789a66f527640b653
hvnovitas-ux <hvnovitas@gmail.com>
2026-07-01 19:04:29 +0200

    Modify authorized email list in login.js
    Updated the list of authorized email addresses for login.

--- login.js
+++ login.js
@@ -1,4 +1,46 @@
+import {
+    auth,
+    provider
+} from "./firebase.js";
+
+import {
+    signInWithPopup,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// Alleen deze e-mailadressen mogen inloggen
 const beheerders = [
-    "jouwmail@gmail.com",
-    "patricia@gmail.com"
+    hvnovitas@gmail.com"
+    // Later voegen we Patricia hier toe
 ];
+
+document.getElementById("login").addEventListener("click", async () => {
+
+    try {
+
+        const result = await signInWithPopup(auth, provider);
+
+        const user = result.user;
+
+        if (beheerders.includes(user.email)) {
+
+            alert("Welkom " + user.displayName);
+
+            // Later sturen we door naar dashboard.html
+            // window.location = "dashboard.html";
+
+        } else {
+
+            alert("Je hebt geen toegang.");
+
+            await signOut(auth);
+
+        }
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
