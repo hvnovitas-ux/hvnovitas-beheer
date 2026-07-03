f745132b27de73f56e9401c678865efc685c1615
hvnovitas-ux <hvnovitas@gmail.com>
2026-07-03 15:30:43 +0200

    Create drive.js

---
+++ drive.js
@@ -0,0 +1,15 @@
+// ==========================================
+// HV NOVITAS CMS - GOOGLE DRIVE
+// ==========================================
+
+export const CLIENT_ID =
+"71716605241-4kgftcmjlen6jakrl7s8mfq2i4dud02r.apps.googleusercontent.com";
+
+export const SCOPES =
+"https://www.googleapis.com/auth/drive.file";
+
+// Map Nieuws
+export const NEWS_FOLDER_ID =
+"1RMn8cqcO-TQBiEHkoAeLBbJTDZApx2uG";
+
+console.log("📷 Google Drive geladen");
