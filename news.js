8cffed7fb9616c51244b8f9cbf70c7f2ae74d8e5
hvnovitas-ux <hvnovitas@gmail.com>
2026-07-03 16:10:18 +0200

    Refactor Google Drive API integration in news.js

--- news.js
+++ news.js
@@ -6,7 +6,7 @@ export const CLIENT_ID =
 "71716605241-4kgftcmjlen6jakrl7s8mfq2i4dud02r.apps.googleusercontent.com";
 
 export const API_KEY =
-"VUL_HIER_JE_BROWSER_API_KEY_IN";
+"AIzaSyC7OnJi3Z5BCaktePTrjb94nZgKMWSwapQ";
 
 export const NEWS_FOLDER_ID =
 "1RMn8cqcO-TQBiEHkoAeLBbJTDZApx2uG";
@@ -14,8 +14,12 @@ export const NEWS_FOLDER_ID =
 export const SCOPES =
 "https://www.googleapis.com/auth/drive.file";
 
-let tokenClient;
+let tokenClient = null;
 let accessToken = null;
+
+// ------------------------------------------
+// Initialiseren
+// ------------------------------------------
 
 export async function initDrive() {
 
@@ -24,15 +28,21 @@ export async function initDrive() {
         gapi.load("client", async () => {
 
             await gapi.client.init({
+
                 apiKey: API_KEY,
+
                 discoveryDocs: [
                     "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
                 ]
+
             });
 
             tokenClient = google.accounts.oauth2.initTokenClient({
+
                 client_id: CLIENT_ID,
+
                 scope: SCOPES,
+
                 callback: (response) => {
 
                     accessToken = response.access_token;
@@ -44,6 +54,7 @@ export async function initDrive() {
                     resolve();
 
                 }
+
             });
 
         });
@@ -51,6 +62,10 @@ export async function initDrive() {
     });
 
 }
+
+// ------------------------------------------
+// Inloggen
+// ------------------------------------------
 
 export async function loginDrive() {
 
@@ -70,8 +85,63 @@ export async function loginDrive() {
 
         };
 
-        tokenClient.requestAccessToken();
+        tokenClient.requestAccessToken({
+            prompt: "consent"
+        });
 
     });
+
+}
+
+// ------------------------------------------
+// Upload afbeelding
+// ------------------------------------------
+
+export async function uploadNewsImage(file) {
+
+    await loginDrive();
+
+    const metadata = {
+
+        name: file.name,
+
+        mimeType: file.type,
+
+        parents: [NEWS_FOLDER_ID]
+
+    };
+
+    const form = new FormData();
+
+    form.append(
+        "metadata",
+        new Blob(
+            [JSON.stringify(metadata)],
+            { type: "application/json" }
+        )
+    );
+
+    form.append("file", file);
+
+    const response = await fetch(
+        "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+        {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            body: form
+        }
+    );
+
+    if (!response.ok) {
+
+        throw new Error(await response.text());
+
+    }
+
+    const data = await response.json();
+
+    return data.id;
 
 }
