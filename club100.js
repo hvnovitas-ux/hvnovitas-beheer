a717c43267ff76ad031bc38bf128adb4057dc0b5
hvnovitas-ux <hvnovitas@gmail.com>
2026-07-25 21:08:00 +0200

    Update console log message and clean up HTML structure

--- club100.js
+++ club100.js
@@ -4,7 +4,7 @@ import {
     onValue
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
-console.log("🏺 Club van 100 geladen");
+console.log("🏺 Club van 100 loaded");
 
 const clubList = document.getElementById("clubList");
 
@@ -38,20 +38,10 @@ onValue(ref(db, "club100"), (snapshot) => {
 
         return `
             <div class="tile">
-
-                <span style="
-                    position:absolute;
-                    top:10px;
-                    left:12px;
-                    font-size:14px;
-                    color:rgba(30,79,138,0.25);
-                ">❖</span>
-
                 <div class="tile-name">
                     <div>${firstName}</div>
                     <div>${lastName}</div>
                 </div>
-
             </div>
         `;
     }).join("");
