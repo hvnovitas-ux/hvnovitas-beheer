886de918e290c6aa100c0b6b20bf32244940271e
hvnovitas-ux <hvnovitas@gmail.com>
2026-07-26 03:40:37 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DB_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDWYYS09i4YN9tnCmAzeiicD9T4YZ3a6HE",
+    authDomain: "hv-novitas-beheer.firebaseapp.com",
+    databaseURL: "https://hv-novitas-beheer-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hv-novitas-beheer",
+    storageBucket: "hv-novitas-beheer.appspot.com",
+    messagingSenderId: "71716605241",
+    appId: "1:71716605241:web:b7e87d680b7499421a6ce8"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
