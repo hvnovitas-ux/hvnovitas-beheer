70ecf3ce2e9bf66255f375e513adf3046c58fbef
hvnovitas-ux <hvnovitas@gmail.com>
2026-07-26 03:03:44 +0200

    Update club100.js to improve member display

--- club100.js
+++ club100.js
@@ -1,7 +1,7 @@
 import { db } from "./firebase.js";
 import { ref, onValue } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
-console.log("🏺 CLUB100 LIVE");
+console.log("🏺 CLUB100 LOADED");
 
 const clubList = document.getElementById("clubList");
 
@@ -10,21 +10,20 @@ onValue(ref(db, "club100"), (snapshot) => {
     const data = snapshot.val();
 
     if (!data) {
-        clubList.innerHTML = "<p>Geen leden</p>";
+        clubList.innerHTML = "<p style='color:white'>Geen leden</p>";
         return;
     }
 
     const items = Object.entries(data);
 
     clubList.innerHTML = items.map(([id, p]) => {
+
         const parts = (p.name || "").split(" ");
 
         return `
             <div class="tile">
-                <div class="tile-text">
-                    <div class="first">${parts[0] || ""}</div>
-                    <div class="last">${parts.slice(1).join(" ")}</div>
-                </div>
+                <div>${parts[0] || ""}</div>
+                <div>${parts.slice(1).join(" ")}</div>
             </div>
         `;
     }).join("");
